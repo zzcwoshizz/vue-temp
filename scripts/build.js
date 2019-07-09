e278b3c2df1d6f67dc195148f28b9817537032a9
jax.wang <1003315987@qq.com>
2019-07-09 11:27:04 +0800

    add cross-env

--- scripts/build.js
+++ scripts/build.js
@@ -1,17 +1,17 @@
-const webpack = require('webpack')
-const config = require('../config/webpack.prod.conf')
+const webpack = require('webpack');
+const config = require('../config/webpack.prod.conf');
 
 webpack(config, (err, stats) => {
   if (err || stats.hasErrors()) {
     // 在这里处理错误
-    console.error(err)
-    return
+    console.error(err);
+    return;
   }
   // 处理完成
   console.log(
     stats.toString({
       chunks: false, // 使构建过程更静默无输出
-      colors: true // 在控制台展示颜色
-    })
-  )
-})
+      colors: true, // 在控制台展示颜色
+    }),
+  );
+});
